ea6dfd0115ca49acae04cc0ec05d32b78c3ca92a
Sonyl Nagale <sonylnagale@users.noreply.github.com>
2024-11-25 18:24:35 -0500

    Initial commit

---
+++ Vehicle.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    def __init__(self, brand, model, year):
+        self._brand = brand
+        self._model = model
+        self._year = year
+
+    def display_info(self):
+        """Abstract method to display vehicle info."""
+        return f"{self._brand} {self._model} ({self._year})"
+    
+    def honk(self):
+        """A general honk sound for all vehicles."""
+        return "Beep beep!"
