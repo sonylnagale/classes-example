e91dbab703fb89c6f18d3507bd6997b84d0a5149
Nader Ammar <naderabdammar@hotmail.com>
2024-11-27 19:13:19 -0500

    Added a new Car class to Vehicle

--- Vehicle.py
+++ Vehicle.py
@@ -11,3 +11,16 @@ class Vehicle:
     def honk(self):
         """A general honk sound for all vehicles."""
         return "Beep beep!"
+    
+class Car(Vehicle):
+    def __init__(self, brand, model, year, number_of_doors):
+        # Call the parent class constructor
+        super().__init__(brand, model, year)
+        self._number_of_doors = number_of_doors
+
+    def honk(self):
+        return "Honk honk! This is a car!"
+
+    def open_trunk(self):
+        return "The trunk is now open."
+
---
+++ vehicl.py
@@ -0,0 +1,11 @@
+[33mcommit 1e4902786719ebbcf8cb87758b81e8c42364f602[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Nader Ammar <naderabdammar@hotmail.com>
+Date:   Wed Nov 27 14:43:14 2024 -0500
+
+    Add Honda class as child of Vehicle
+
+[33mcommit ea6dfd0115ca49acae04cc0ec05d32b78c3ca92a[m
+Author: Sonyl Nagale <sonylnagale@users.noreply.github.com>
+Date:   Mon Nov 25 18:24:35 2024 -0500
+
+    Initial commit
